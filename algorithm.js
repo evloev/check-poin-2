7783860394d6292d6a609931d63fe80a46a3a99c
abdullah <abdullah.evloev.06@mail.ru>
2021-06-28 18:29:33 +0300

    commit-5

--- algorithm.js
+++ algorithm.js
@@ -2,6 +2,16 @@ function getDayName (day) {
    if (typeof day !== 'number') {
       return 'error'
    }
+   
+
+   if(day > 0 && day < 8) {
+      return 'все верно'
+   }
+
+   else {
+      return 'неверные данные на входе'
+   }
+
 
 }
-console.log(getDayName('fsdfsd'))
+ console.log(getDayName(5))
